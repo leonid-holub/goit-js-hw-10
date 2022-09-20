f56e547dce74d55feb5fe780ba4476f2b854d1de
Leonid Holub <rsleonid@gmail.com>
2022-09-20 13:31:32 +0300

    Update js.

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,7 @@
-export default refs = {
+const refs = {
   inputCountry: document.querySelector('#search-box'),
   countryList: document.querySelector('ul.country-list'),
   countryInfo: document.querySelector('div.country-info'),
 };
+
+export default refs;
